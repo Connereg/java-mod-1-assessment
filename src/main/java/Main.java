3308c9dbcbb16081ee04e2b06f1a9151d121aa2e
Conner Gleason <connereg@optonline.net>
2022-06-24 13:09:41 -0400

    Almost finished all Core Deliverables

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
 
+class DealerNumContainer {
+    public int number;
+}
+
+class TryAgainContainer {
+    public boolean bool;
+}
+
+
 public class Main {
     public static void main(String[] args) {
         //Start Game with a standard greeting for all players
         System.out.println("Hello Traveler! I want to play a guessing game with you, let us take a look at the different kinds of rules we can play by!");
-        boolean tryAgain = true;
+        TryAgainContainer tryAgainOption = new TryAgainContainer();
+
+        Scanner tryAgainScanner = new Scanner(System.in);
+        tryAgainOption.bool = true;
         
-        while (tryAgain) {
+        while (tryAgainOption.bool == true) {
         //(Path: AB) - (Path:A) Create Difficulty Option Selection + (Path B:) Explanation of the Different Rules for each Game
             difficultyOptionSelector();
             Scanner difficultyScanner = new Scanner(System.in);
@@ -34,7 +46,8 @@ public class Main {
             
             else if (difficultyInput == 2) {
                 //Easy Mode
-                System.out.println("I see you have chose Easy mode! Looks like someone likes to win ;) ");
+                System.out.println("___________________________________");
+                System.out.println("I see you have chosen Easy mode! Looks like someone likes to win ;) ");
                 System.out.println("I will now think of a number between 1 and 10 and keep it hidden from you...");
                 System.out.println("Ah yes. This number will do well I think!");
                 System.out.println("Now it is time to guess! You must give me a number greater than or equal to my number!");
@@ -43,15 +56,19 @@ public class Main {
                 // TIME FOR GAME FUNCTION: EASYMODE
                 Scanner easyUserInputScanner = new Scanner(System.in);
                 int easyUserInputNum = easyUserInputScanner.nextInt();
-                int dealerNum = 0;
-                randomNumberGenerator(dealerNum);
-                System.out.println("This is the dealer number: " + dealerNum);
-                tryAgain = false;
+                DealerNumContainer dealerNumber1 = new DealerNumContainer();
+                randomNumberGenerator(dealerNumber1);
+                comparisonEvaluator(difficultyInput, dealerNumber1.number, easyUserInputNum);
+                int tryAgainSelectionNum = tryAgainScanner.nextInt(); 
+                tryAgainFunc(tryAgainSelectionNum, tryAgainOption);
+                
+                
                 
             }
             else if (difficultyInput == 3) {
                 //Medium Mode
-                System.out.println("I see you have chose Medium mode! This may be harder than you think! ");
+                System.out.println("___________________________________");
+                System.out.println("I see you have chosen Medium mode! This may be harder than you think! ");
                 System.out.println("I will now think of a number between 1 and 10 and keep it hidden from you...");
                 System.out.println("Ah yes. This number will do well I think!");
                 System.out.println("Now it is time to guess! You must give me a number strictly greater than my number!");
@@ -60,19 +77,30 @@ public class Main {
                 // TIME FOR GAME FUNCTION: MEDIUMMODE
                 Scanner mediumUserInputScanner = new Scanner(System.in);
                 int mediumUserInputNum = mediumUserInputScanner.nextInt();
+                DealerNumContainer dealerNumber2 = new DealerNumContainer();
+                randomNumberGenerator(dealerNumber2);
+                comparisonEvaluator(difficultyInput, dealerNumber2.number, mediumUserInputNum);
+                int tryAgainSelectionNum = tryAgainScanner.nextInt();
+                tryAgainFunc(tryAgainSelectionNum, tryAgainOption);
 
             }
             else if (difficultyInput == 4) {
                 //Hard Mode
-                System.out.println("I see you have chose Hard mode! You are definitely brave I will give you that...");
+                System.out.println("___________________________________");
+                System.out.println("I see you have chosen Hard mode! You are definitely brave I will give you that...");
                 System.out.println("I will now think of a number between 1 and 10 and keep it hidden from you...");
                 System.out.println("Ah yes. This number will do well I think!");
-                System.out.println("Now it is time to guess! You must give me a number greater than or equal to my number!");
+                System.out.println("Now it is time to guess! You must give me a number EXACTLY EQUAL to my number!");
                 System.out.println("What number will you choose? (1-10): ");
                 System.out.println("___________________________________");
                 // TIME FOR GAME FUNCTION: HARDMODE
                 Scanner hardUserInputScanner = new Scanner(System.in);
                 int hardUserInputNum = hardUserInputScanner.nextInt();
+                DealerNumContainer dealerNumber3 = new DealerNumContainer();
+                randomNumberGenerator(dealerNumber3);
+                comparisonEvaluator(difficultyInput, dealerNumber3.number, hardUserInputNum);
+                int tryAgainSelectionNum = tryAgainScanner.nextInt();
+                tryAgainFunc(tryAgainSelectionNum, tryAgainOption);
 
             }
             else {
@@ -88,9 +116,9 @@ public class Main {
 
         //(Path: A1 -> A2 OR AB) User inputs a valid selection for difficulty OTHERWISE ask for difficulty option again
 
-        //(Path: A2 -> A3) System determines hidden number and lets player know it is doing so
+        //(Path: A2 -> A3) System asks for guess input from User and stores value
 
-        //(Path: A3 -> A4) System asks for guess input from User and stores value
+        //(Path: A3 -> A4) System determines hidden number and lets player know it is doing so
 
         //(Path: A4 -> A5) System evaluates winner via difficulty rules (Different evaluation methods for each difficulty)
 
@@ -99,9 +127,10 @@ public class Main {
         //(Path: A6 -> AB OR EXIT) Asks user if they would like to restart the game, answer dictates either loop or EXIT program)
         }
 
-    }
+    } //MAIN END
 
     public static void difficultyOptionSelector() {
+        System.out.println("___________________________________");
         System.out.println("Choose one of the following options to proceed with our game: ");
         System.out.println("1. Explanation of Different Difficulty Rulesets ");
         System.out.println("Start the game with the following difficulty: ");
@@ -112,6 +141,7 @@ public class Main {
         System.out.println("_____________________");
     }
     public static void difficultyExplanationSelector() {
+        System.out.println("___________________________________");
         System.out.println("Choose one of the following options to hear about the different rulesets: ");
         System.out.println("1. Explain Easy Mode Rules");
         System.out.println("2. Explain Medium Mode Rules");
@@ -120,37 +150,99 @@ public class Main {
         System.out.println("_____________________");
     }
     public static void ruleset1() {
+        System.out.println("___________________________________");
         System.out.println(" EASY MODE RULES: ");
-        System.out.println("__________________");
         System.out.println("You will guess a number between 1 and 10, the if your number is GREATER THAN OR EQUAL TO my hidden number you win the game");
         System.out.println("If your number is less than mine however, you will lose!");
-        System.out.println("__________________");
     }
     public static void ruleset2() {
+        System.out.println("___________________________________");
         System.out.println(" MEDIUM MODE RULES: ");
-        System.out.println("__________________");
         System.out.println("You will guess a number between 1 and 10, the if your number is GREATER THAN my hidden number you win the game");
         System.out.println("If your number is less than or equal to mine however, you will lose!");
-        System.out.println("__________________");
     }
     public static void ruleset3() {
+        System.out.println("___________________________________");
         System.out.println(" HARD MODE RULES: ");
-        System.out.println("__________________");
         System.out.println("You will guess a number between 1 and 10, the if your number is EXACTLY EQUAL TO my hidden number you win the game");
         System.out.println("If your number is less or greater than mine however, you will lose!");
-        System.out.println("__________________");
     }
     public static void invalidInputError() {
+        System.out.println("_________________________");
         System.out.println("This is not a valid input! Please refer to the options listed and select the number for the option you wish to choose");
         System.out.println("_________________________");
     }
-    public static void randomNumberGenerator(int returnedNum) {
+    public static void randomNumberGenerator(DealerNumContainer returnedNum) {
         int max = 10;
         int min = 1;
         int range = (max - min) + 1;
-        int randomResult = (int) (Math.random() * range) + min;
-        returnedNum = randomResult;
-        System.out.println("This is the dealer num INSIDE function: " + returnedNum);
-
+        returnedNum.number = (int) (Math.random() * range) + min;
     }
-}
+    public static void comparisonEvaluator(int difficultyModeSelected, int finalDealerNum, int finalPlayerNum) {
+        if (difficultyModeSelected == 2) {
+            if (finalPlayerNum >= finalDealerNum) {
+                System.out.println("Drats! You have picked " + finalPlayerNum + " and I picked " + finalDealerNum + "...");
+                System.out.println("You foiled me! your number is greater than or equal to my number.");
+                System.out.println("YOU WIN!");
+                playAgainInstructions();
+            }
+            else {
+                System.out.println("HA! Nice try, but you picked " + finalPlayerNum + " and I picked " + finalDealerNum + "!");
+                System.out.println("Looks like YOU LOST! But fear not, you can always try again if your feeling lucky!");
+                playAgainInstructions();
+            }
+        }
+        else if (difficultyModeSelected == 3) {
+            if (finalPlayerNum > finalDealerNum) {
+                System.out.println("Drats! You have picked " + finalPlayerNum + " and I picked " + finalDealerNum + "...");
+                System.out.println("You foiled me! your number is greater than my number.");
+                System.out.println("YOU WIN!");
+                playAgainInstructions();
+            }
+            else {
+                System.out.println("HA! Nice try, but you picked " + finalPlayerNum + " and I picked " + finalDealerNum + "!");
+                System.out.println("Looks like YOU LOST! But fear not, you can always try again if your feeling lucky!");
+                playAgainInstructions();  
+            }
+        }
+        else if (difficultyModeSelected == 4) {
+            if (finalPlayerNum == finalDealerNum) {
+                System.out.println("Drats! You have picked " + finalPlayerNum + " and I picked " + finalDealerNum + "...");
+                System.out.println("You foiled me! your number is exactly equal to my number.");
+                System.out.println("YOU WIN!");
+                playAgainInstructions();
+
+            }
+            else {
+                System.out.println("HA! Nice try, but you picked " + finalPlayerNum + " and I picked " + finalDealerNum + "!");
+                System.out.println("Looks like YOU LOST! But fear not, you can always try again if your feeling lucky!");
+                playAgainInstructions(); 
+            }
+        }
+        else {
+            System.out.println("_________________________");
+            System.out.println("This is not a valid input! Please refer to the options listed and select the number for the option you wish to choose");
+            System.out.println("Let's try again from the top... Maybe try reading the rules Traveler?");
+            System.out.println("_________________________");
+            return;
+        }
+    }
+    public static void tryAgainFunc(int playerChoice, TryAgainContainer tryAgainOption) {
+        if (playerChoice == 1) {
+            tryAgainOption.bool = true;
+        }
+        else if (playerChoice == 2) {
+            System.out.println("Thanks for playing! Wishing you the best of luck in the future!");
+            tryAgainOption.bool = false;
+        }
+    }
+    public static void playAgainInstructions() {
+        System.out.println("________________________");
+        System.out.println("Would you Like to play again?");
+        System.out.println("1. Yes I would!");
+        System.out.println("2. No, I am fine for now thanks!");
+        System.out.println("_________________________");
+    }
+} //END OF FILE
+
+//
